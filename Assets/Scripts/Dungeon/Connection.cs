e4f7371d75ce695e058a82783e2b62d0693a042d
David <synlike@gmail.com>
2021-11-17 09:41:05 +0100

    Génération des salles en texte

--- Assets/Scripts/Dungeon/Connection.cs
+++ Assets/Scripts/Dungeon/Connection.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class Connection : MonoBehaviour
 {
     Node[] nodes = new Node[2];
+    public Node[] Nodes
+    {
+        get { return nodes; }
+        set { nodes = value; }
+    }
 
-    public void SetNodes(Node node1, Node node2)
+    public Connection(Node node1, Node node2)
     {
         nodes[0] = node1;
         nodes[1] = node2;
